e8889be5af39b82de2dd1d04965d12998fa8d89a
MOHAMMEDANSILKA <9656611143ansil@gmail.com>
2025-01-24 23:55:04 +0530

    port seting done

--- server.js
+++ server.js
@@ -1,3 +1,34 @@
 const express =require ('express');
+const app= express();
+app.use(express.json);
+
+const PORT =process.env.PORT || 3000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
