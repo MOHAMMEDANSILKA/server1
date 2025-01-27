0a8c6b4fb73bb04b72d8392bdfe018445a3a636f
MOHAMMEDANSILKA <9656611143ansil@gmail.com>
2025-01-28 01:46:46 +0530

    many changes made

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express =require ('express');
 const app= express();
-app.use(express.json);
+app.use(express.json());
 
 const PORT =process.env.PORT || 3000;
-app.listen(PORT,()=>{
-  console.log("server listening in PORT:",port)
+
+let books =[{
+  id:1,title:"alchemist", author:"paulo"
+},{id:2,title:"elon musk", author:"ashlee vance"},{id:3,title:"rich dad", author:"robert"}];
+// getting books
+app.get('/books',(req,res)=>{
+res.json(books);
+
+})
+//post a new book
+app.post('/books',(req,res)=>{
+  const book =req.body;
+  book.id =books.length+1;
+  books.push(book);
+  res.status(201).json(book);
 });
+//start the server
+app.listen(3000,()=>{
+  console.log("server running on local host 3000")
+})
+
 
 
 
