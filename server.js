3caf4c446865379ca6c152e93f5fc108de3d03cc
MOHAMMEDANSILKA <9656611143ansil@gmail.com>
2025-01-30 00:02:14 +0530

    mongodb connected

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const app=express();
 const mongoose=require('mongoose');
+//code for mongodb
 mongoose.connect('mongodb://localhost/subscribers')
 app.listen(3000,()=>console.log("server is started"));
+
