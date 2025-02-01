c1d2420e203b3d1f139735063c6d78ca45585d38
MOHAMMEDANSILKA <9656611143ansil@gmail.com>
2025-02-02 00:25:13 +0530

    chamges

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,3 +1,24 @@
 const express=require('express');
 const router=express.Router();
-module.export=router;
+
+// getting all
+router.get('/', (req,res)=>{
+    res.send("hellow");
+});
+// getting one
+router.get('/:id', (req,res)=>{
+
+});
+//creating one
+router.post('/', (req,res)=>{
+
+});
+//updating one
+router.patch('/:id', (req,res)=>{
+
+});
+//deleting one
+router.delete('/:id', (req,res)=>{
+
+});
+module.exports=router; 
