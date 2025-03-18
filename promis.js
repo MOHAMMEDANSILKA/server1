796c4fb680f8400a58f376ebb55925300ce533f8
MOHAMMEDANSILKA <98404390+MOHAMMEDANSILKA@users.noreply.github.com>
2025-03-18 23:54:37 +0530

    Update promis.js
    written promise

--- promis.js
+++ promis.js
@@ -2,4 +2,8 @@ const cart=["paste","juice","milk"];
 createOrder(cart, function(orderId){
   proceedToPayment(orderId);
 });
+const promise =createOrder(cart);
+promise.then(function(orderId){
+  proceedToPayment(orderId);
+});
  
