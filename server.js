7bd2b0cddfc22b6b781fe39648ad2dad5cf11b16
MOHAMMEDANSILKA <98404390+MOHAMMEDANSILKA@users.noreply.github.com>
2025-03-21 01:38:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose=require('mongoose');
 
 mongoose.connect('mongodb://localhost/subscribers');
 const db=mongoose.connnection;
-db
+
 
 app.listen(3000,()=>console.log("server is started"));
 
